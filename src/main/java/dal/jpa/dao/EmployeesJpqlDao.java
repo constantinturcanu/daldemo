24bc7188f2f593c7638daa47d045047e01602e60
Constantin Turcanu <constantin.turcanu.sdet@gmail.com>
2020-06-10 20:40:27 +0300

    Added JPQL Demo tests and DAO methods.

--- src/main/java/dal/jpa/dao/EmployeesJpqlDao.java
+++ src/main/java/dal/jpa/dao/EmployeesJpqlDao.java
@@ -2,44 +2,156 @@ package dal.jpa.dao;
 
 import dal.jpa.entities.Employee;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
+// find more JPQL examples at https://en.wikibooks.org/wiki/Java_Persistence/JPQL
 public class EmployeesJpqlDao {
 
-    public static EntityManager entitymanager;
+    public static final String JPA_DEMO_PERSISTENCE_UNIT = "JPA_Demo";
+
+    public static EntityManagerFactory entityManagerFactory;
 
     static {
-        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "JPA_Demo" );
-        entitymanager = emfactory.createEntityManager();
+        entityManagerFactory = Persistence.createEntityManagerFactory(JPA_DEMO_PERSISTENCE_UNIT);
     }
 
-    public static void insertEmployee() {
-
+    // There's no INSERT statement in JPQL.
+    // The workaround is to use createNativeQuery()
+    //  - note the use of EntityTransaction
+    //    and actual table name and column names, instead of Entity class name and fields
+    public static int insertEmployee(Date birthDate,
+                                     String firstName,
+                                     String lastName,
+                                     String gender,
+                                     Date hireDate) {
+        int empNo = getNextAvailableEmployeeNumber();
+        String query = "INSERT INTO employees (emp_no, birth_date, first_name, last_name, gender, hire_date) " +
+                "VALUES (?,?,?,?,?,?)";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entitymanager.getTransaction();
+        entityTransaction.begin();
+        entitymanager.createNativeQuery(query)
+                .setParameter(1, empNo)
+                .setParameter(2, birthDate)
+                .setParameter(3, firstName)
+                .setParameter(4, lastName)
+                .setParameter(5, gender)
+                .setParameter(6, hireDate)
+                .executeUpdate();
+        entityTransaction.commit();
+        entitymanager.close();
+        return empNo;
     }
 
-    public static List<Employee> selectEmployeeByFirstName(String firstName) {
-        Query query = entitymanager.createQuery("SELECT e FROM Employee e WHERE e.firstName='" + firstName + "'");
-        List<Employee> employees = query.getResultList();
+    public static Employee selectEmployeeWhereEmployeeNumber(int employeeNumber) {
+        String query = "SELECT e FROM Employee e WHERE e.empNo=" + employeeNumber + "";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        Employee employee = entitymanager
+                .createQuery(query, Employee.class)
+                .getSingleResult();
+        entitymanager.close();
+        return employee;
+    }
+
+    public static List<Employee> selectEmployeeWhereFirstName(String firstName) {
+        String query = "SELECT e FROM Employee e WHERE e.firstName='" + firstName + "'";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        List<Employee> employees = entitymanager
+                .createQuery(query)
+                .getResultList();
+        entitymanager.close();
         return employees;
     }
 
-    public static List<Employee> selectEmployeeByFirstNameAndLastName(String firstName, String lastName) {
-        Query query = entitymanager.createQuery("SELECT e FROM Employee e WHERE e.firstName='" + firstName + "'" +
-                " AND e.lastName='" + lastName + "'");
-        List<Employee> employees = query.getResultList();
+    public static List<Employee> selectEmployeeWhereFirstNameAndLastName(String firstName, String lastName) {
+        String query = "SELECT e FROM Employee e WHERE e.firstName='" + firstName + "' AND e.lastName='" + lastName + "'";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        List<Employee> employees = entitymanager.createQuery(query).getResultList();
+        entitymanager.close();
         return employees;
     }
 
-    public static void updateEmployee() {
-
+    public static List<Employee> selectEmployeeWhereFirstNameLike(String firstName) {
+        String query = "SELECT e FROM Employee e WHERE e.firstName LIKE '%" + firstName + "%'";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        List<Employee> employees = entitymanager
+                .createQuery(query)
+                .getResultList();
+        entitymanager.close();
+        return employees;
     }
 
-    public static void deleteEmployee() {
+    // EntityTransaction is required
+    // Query is build with named parameters
+    public static int updateEmployeeLastNameWhereFirstNameAndLastName(String firstName,
+                                                                       String lastName,
+                                                                       String newLastName) {
+        String query = "UPDATE Employee e" +
+                " SET e.lastName=:newLastName" +
+                " WHERE e.firstName=:firstName" +
+                " AND e.lastName=:lastName";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entitymanager.getTransaction();
+        entityTransaction.begin();
+        int updatedRowsCount = entitymanager.createQuery(query)
+                .setParameter("newLastName", newLastName)
+                .setParameter("firstName", firstName)
+                .setParameter("lastName", lastName)
+                .executeUpdate();
+        entityTransaction.commit();
+        entitymanager.close();
+        return updatedRowsCount;
+    }
 
+    public static int deleteEmployeeWhereFirstNameAndLastName(String firstName, String lastName ) {
+        String query = "DELETE FROM Employee e" +
+                " WHERE e.firstName='" + firstName + "' "+
+                " AND e.lastName='" + lastName + "'";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        int deletedRowsCount = entitymanager.createQuery(query).executeUpdate();
+        entitymanager.close();
+        return deletedRowsCount;
+    }
+
+    public static int deleteEmployeeWhereEmployeeNumber(int employeeNumber) {
+        String query = "DELETE FROM Employee e WHERE e.empNo=:employeeNumber";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entitymanager.getTransaction();
+        entityTransaction.begin();
+        int deletedRowsCount = entitymanager.createQuery(query)
+                .setParameter("employeeNumber", employeeNumber)
+                .executeUpdate();
+        entityTransaction.commit();
+        entitymanager.close();
+        return deletedRowsCount;
+    }
+
+    public static int getNextAvailableEmployeeNumber() {
+        return getLastEmployeeNumber() + 1;
+    }
+
+    public static int getLastEmployeeNumber() {
+        String query = "SELECT MAX(e.empNo) FROM Employee e";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entitymanager.getTransaction();
+        entityTransaction.begin();
+        int lastEmployeeNumberId = entitymanager.createQuery(query, Integer.class)
+                .getSingleResult();
+        entityTransaction.commit();
+        entitymanager.close();
+        return lastEmployeeNumberId;
+    }
+
+    public static boolean isEmployeeInDatabaseByEmployeeNumber(int employeeNumber) {
+        String query = "SELECT COUNT(e) FROM Employee e WHERE e.empNo=:employeeNumber";
+        EntityManager entitymanager = entityManagerFactory.createEntityManager();
+        long foundEmployeesCount = entitymanager.createQuery(query, Long.class)
+                .setParameter("employeeNumber", employeeNumber)
+                .getSingleResult();
+        entitymanager.close();
+        return foundEmployeesCount > 0;
     }
 
 }
--- src/test/java/jpahibernatedemo/JpqlDemo.java
+++ src/test/java/jpahibernatedemo/JpqlDemo.java
@@ -2,38 +2,74 @@ package jpahibernatedemo;
 
 import dal.jpa.dao.EmployeesJpqlDao;
 import dal.jpa.entities.Employee;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class JpqlDemo {
+    DateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     // CREATE
     @Test
-    public void createEmployeeJpqlTest() {
+    public void createEmployeeJpqlTest() throws ParseException {
+        Date birthDate = simpleDateFormat.parse("1990-1-30");
+        String firstName = "Demo_Employee";
+        String lastName = "Jpql_Create";
+        String gender = "F";
+        Date hireDate = simpleDateFormat.parse("2020-6-10");
 
+        int createdEmployeeNumber = EmployeesJpqlDao.insertEmployee(birthDate, firstName, lastName, gender, hireDate);
+
+        Employee actualEmployee = EmployeesJpqlDao.selectEmployeeWhereEmployeeNumber(createdEmployeeNumber);
+
+        Assert.assertNotNull(actualEmployee, "The employee was not created successfully in database.");
     }
 
     // READ
     @Test
     public void readEmployeeJpqlTest() {
-        List<Employee> employeeList = EmployeesJpqlDao.selectEmployeeByFirstName("Lillian");
-        String lastname = employeeList.get(0).getLastName();
-        System.out.println("lastname = " + lastname);
-        List<Employee> employeeList2 = EmployeesJpqlDao.selectEmployeeByFirstNameAndLastName("Lillian", "Haddadi");
-        System.out.println(employeeList2.get(0));
+        String firstName = "Lillian";
+        String lastName = "Haddadi";
+
+        List<Employee> foundEmployees = EmployeesJpqlDao.selectEmployeeWhereFirstNameAndLastName(firstName, lastName);
+
+        Assert.assertTrue(foundEmployees.size() > 0,
+                String.format("Found no employee named %s %s.", firstName, lastName) );
     }
 
     // UPDATE
     @Test
     public void updateEmployeeJpqlTest() {
+        int lastEmployeeNumber = EmployeesJpqlDao.getLastEmployeeNumber();
+        Employee lastAddedEmployee = EmployeesJpqlDao.selectEmployeeWhereEmployeeNumber(lastEmployeeNumber);
+        String newLastName = "Jpql_Update";
+
+        EmployeesJpqlDao.updateEmployeeLastNameWhereFirstNameAndLastName(
+                lastAddedEmployee.getFirstName(),
+                lastAddedEmployee.getLastName(),
+                newLastName);
+
+        Employee updatedEmployee = EmployeesJpqlDao.selectEmployeeWhereEmployeeNumber(lastEmployeeNumber);
+
+        Assert.assertEquals(updatedEmployee.getLastName(), newLastName,
+                "The last_name column was not updated for employee.");
 
     }
 
     // DELETE
     @Test
     public void deleteEmployeeJpqlTest() {
+        int lastEmployeeNumber = EmployeesJpqlDao.getLastEmployeeNumber();
+        int deletedRowsCount = EmployeesJpqlDao.deleteEmployeeWhereEmployeeNumber(lastEmployeeNumber);
 
+        Assert.assertEquals(deletedRowsCount, 1, "No rows were deleted.");
+        Assert.assertTrue(!EmployeesJpqlDao.isEmployeeInDatabaseByEmployeeNumber(lastEmployeeNumber),
+                "Employee with employeeNumber " + lastEmployeeNumber + " still exists in database.");
     }
 
 }
