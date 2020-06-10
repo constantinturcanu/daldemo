0dbfee829482db3e95e7759c4cdcbaaf4663a0c6
Constantin Turcanu <constantin.turcanu.sdet@gmail.com>
2020-06-10 23:56:47 +0300

    Added JOOQ Demo tests and DAO methods.

--- src/main/java/dal/jooq/dao/EmployeesJooqDao.java
+++ src/main/java/dal/jooq/dao/EmployeesJooqDao.java
@@ -1,27 +1,111 @@
 package dal.jooq.dao;
 
-import dal.jpa.entities.Employee;
+import dal.jooq.entities.enums.EmployeesGender;
+import dal.jooq.entities.tables.Employees;
+import dal.jooq.entities.tables.records.EmployeesRecord;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 import javax.persistence.Query;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+
+import static dal.jooq.entities.tables.Employees.EMPLOYEES_;
 
 public class EmployeesJooqDao {
 
+    private static DSLContext dslContext;
 
-    public static void insertEmployee() {
+    static {
+        String mysqlUsername = "demo_user";
+        String mysqlPassword = "parola";
+        String mysqlUrl = "jdbc:mysql://192.168.200.55:3306/employees";
 
+        try {
+            Connection mysqlConnection = DriverManager.getConnection(mysqlUrl, mysqlUsername, mysqlPassword);
+            dslContext = DSL.using(mysqlConnection, SQLDialect.MYSQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public static void selectEmployee() {
 
+    public static EmployeesRecord storeEmployee(Integer employeeNumber,
+                                                LocalDate birthDate,
+                                                String firstName,
+                                                String lastName,
+                                                EmployeesGender gender,
+                                                LocalDate hireDate) {
+        EmployeesRecord newEmployee = dslContext.newRecord(EMPLOYEES_);
+        newEmployee.setEmpNo(employeeNumber);
+        newEmployee.setBirthDate(birthDate);
+        newEmployee.setFirstName(firstName);
+        newEmployee.setLastName(lastName);
+        newEmployee.setGender(gender);
+        newEmployee.setHireDate(hireDate);
+        newEmployee.store();
+
+        return newEmployee;
     }
 
-    public static void updateEmployee() {
+    public static List<Record> selectEmployeeByFirstNameAndLastName(String firstName, String lastName) {
+        Result<Record> employees = dslContext
+                .select()
+                .from(EMPLOYEES_)
+                .where(EMPLOYEES_.FIRST_NAME.eq(firstName))
+                .and(EMPLOYEES_.LAST_NAME.eq(lastName))
+                .fetch();
 
+        return employees;
     }
 
-    public static void deleteEmployee() {
-
+    public static EmployeesRecord selectEmployeeByEmployeeNumber(int employeeNumber) {
+        return  (EmployeesRecord) dslContext
+                .select()
+                .from(EMPLOYEES_)
+                .where(EMPLOYEES_.EMP_NO.eq(employeeNumber))
+                .fetchAny();
     }
 
+    public static int updateEmployeeLastNameWhereEmployeeNumber(Integer employeeNumber, String newLastName) {
+        int numberOfUpdatedRows = dslContext
+                .update(EMPLOYEES_)
+                .set(EMPLOYEES_.LAST_NAME, newLastName)
+                .where(EMPLOYEES_.EMP_NO.eq(employeeNumber))
+                .execute();
+
+        return numberOfUpdatedRows;
+    }
+
+    public static int deleteEmployee(int employeeNumber) {
+        int numberOfDeletedRows = dslContext
+                .delete(EMPLOYEES_)
+                .where(EMPLOYEES_.EMP_NO.eq(employeeNumber))
+                .execute();
+
+        return numberOfDeletedRows;
+    }
+
+    public static Integer getLastEmployeeNumber() {
+        return dslContext
+                .select(DSL.max(EMPLOYEES_.EMP_NO))
+                .from(EMPLOYEES_)
+                .fetchAny()
+                .value1();
+    }
+
+    public static Integer getNextAvailableEmployeeNumber() {
+        return getLastEmployeeNumber() + 1;
+    }
+
+    public static boolean isEmployeeInDatabase(Integer employeeNumber) {
+        return selectEmployeeByEmployeeNumber(employeeNumber) != null;
+    }
 }
--- src/test/java/jooqdemo/JooqDemo.java
+++ src/test/java/jooqdemo/JooqDemo.java
@@ -1,31 +1,89 @@
 package jooqdemo;
 
+import dal.jooq.dao.EmployeesJooqDao;
+import dal.jooq.entities.enums.EmployeesGender;
+import dal.jooq.entities.tables.records.EmployeesRecord;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.LocalDate;
 
 public class JooqDemo {
 
     // CREATE
     @Test
     public void createEmployeeJooqTest() {
+        Integer employeeNumber = EmployeesJooqDao.getNextAvailableEmployeeNumber();
+        LocalDate birthDate = LocalDate.of(1979, 8, 24);
+        String firstName = "Demo_Employee";
+        String lastName = "Jooq_Create";
+        EmployeesGender gender = EmployeesGender.M;
+        LocalDate hireDate = LocalDate.of(1969, 7, 27);
+
+        EmployeesJooqDao.storeEmployee(
+                employeeNumber,
+                birthDate,
+                firstName,
+                lastName,
+                gender,
+                hireDate);
+
+        EmployeesRecord createdEmployee = EmployeesJooqDao.selectEmployeeByEmployeeNumber(employeeNumber);
+
+        System.out.println("createdEmployee");
+        System.out.println(createdEmployee);
+
+        Assert.assertEquals(createdEmployee.getEmpNo(), employeeNumber, "employeeNumber is not correctly stored in database.");
+        Assert.assertEquals(createdEmployee.getBirthDate(), birthDate, "birthDate is not correctly stored in database.");
+        Assert.assertEquals(createdEmployee.getFirstName(), firstName, "firstName is not correctly stored in database.");
+        Assert.assertEquals(createdEmployee.getLastName(), lastName, "lastName is not correctly stored in database.");
+        Assert.assertEquals(createdEmployee.getGender(), gender, "gender is not correctly stored in database.");
+        Assert.assertEquals(createdEmployee.getHireDate(), hireDate, "hireDate is not correctly stored in database.");
 
     }
 
     // READ
     @Test
     public void readEmployeeJooqTest() {
+        Integer employeeNumber = EmployeesJooqDao.getLastEmployeeNumber();
+        EmployeesRecord lastCreatedEmployee = EmployeesJooqDao.selectEmployeeByEmployeeNumber(employeeNumber);
 
+        System.out.println("lastCreatedEmployee");
+        System.out.println(lastCreatedEmployee);
+
+        Assert.assertEquals(lastCreatedEmployee.getEmpNo(), employeeNumber, "employeeNumber is not correctly stored in database.");
     }
 
     // UPDATE
     @Test
     public void updateEmployeeJooqTest() {
+        Integer employeeNumber = EmployeesJooqDao.getLastEmployeeNumber();
+        EmployeesRecord employeeToUpdate = EmployeesJooqDao.selectEmployeeByEmployeeNumber(employeeNumber);
+        System.out.println("employeeToUpdate");
+        System.out.println(employeeToUpdate);
 
+        String newLastName = "Jooq_Update";
+        int updatedRowsCount = EmployeesJooqDao.updateEmployeeLastNameWhereEmployeeNumber(employeeNumber, newLastName);
+        EmployeesRecord updatedEmployee = EmployeesJooqDao.selectEmployeeByEmployeeNumber(employeeNumber);
+        System.out.println("updatedEmployee");
+        System.out.println(updatedEmployee);
+
+        Assert.assertEquals(updatedRowsCount, 1, "Unexpected number of updated rows.");
+        Assert.assertEquals(updatedEmployee.getLastName(), newLastName, "Employee last name was not updated in database.");
     }
 
     // DELETE
     @Test
     public void deleteEmployeeJooqTest() {
+        Integer employeeNumber = EmployeesJooqDao.getLastEmployeeNumber();
 
+        int numberOfDeletedRows = EmployeesJooqDao.deleteEmployee(employeeNumber);
+
+        boolean isUserInDatabase = EmployeesJooqDao.isEmployeeInDatabase(employeeNumber);
+
+        Assert.assertEquals(numberOfDeletedRows, 1, "Unexpected number of rows were deleted.");
+        Assert.assertEquals(isUserInDatabase, false,
+                "The user with employee number " + employeeNumber + " is still in database.");
     }
 
 
